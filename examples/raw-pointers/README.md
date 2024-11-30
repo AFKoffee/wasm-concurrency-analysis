207b6d0ad6bc54ba2bdaa0282272b7e4f038f170
AFKoffee <AFKoffee@protonmail.com>
2024-11-30 21:54:45 +0100

    add a deadlocking example using raw pointers

---
+++ examples/raw-pointers/README.md
@@ -0,0 +1,10 @@
+# Wasm Deadlock Example - Raw Pointers
+This crate serves as an example of a deadlocking Wasm module using raw pointers in Rust.
+
+## Getting started
+1. Make sure to have the Rust programming language installed (rustc and cargo)
+2. Make sure to have the `wasm-pack` command line tool installed
+3. Make sure to have python installed (needed for local web server)
+4. To build the project execute `wasm-pack build --target no-modules` in the project root directory
+5. Start the python web server `python server.py` also from the project root directory
+6. Navigate to `localhost:8000` in your browser to see the demo. (Important: Use _localhost_ in the URL. Otherwise the http-headers needed for shared wasm-memory may not work correctly - tested on Firefox)
---
+++ examples/raw-pointers/src/lib.rs
@@ -0,0 +1,114 @@
+use wasm_bindgen::prelude::*;
+use web_sys::Worker;
+use std::sync::{Mutex, MutexGuard};
+
+macro_rules! console_log {
+    ($($t:tt)*) => (crate::log(&format_args!($($t)*).to_string()))
+}
+
+mod spawner;
+//mod threadpool;
+
+struct Work {
+    func: Box<dyn FnOnce() -> Result<(), JsValue> /* + Send */>,
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+    
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn logv(x: &JsValue);
+}
+
+#[wasm_bindgen]
+pub async  fn create_deadlock() -> Result<(), JsValue> {
+    console_log!("Main: starting attempt to deadlock.");
+    let worker1 = spawner::spawn()?;
+    let worker2 = spawner::spawn()?;
+
+    let data1 = Box::new(SyncronizedData::default());
+    let data2 = Box::new(SyncronizedData::default());
+
+    let ptr1 = Box::into_raw(data1);
+    let ptr2 = Box::into_raw(data2);
+
+    let work1 = Box::new(Work { func: Box::new(move || {
+        deadlock_prone_task(unsafe {&Box::from_raw(ptr1)}, unsafe {&Box::from_raw(ptr2)}, 1)
+    }) });
+
+    let work2 = Box::new(Work { func: Box::new(move || {
+        deadlock_prone_task(unsafe {&Box::from_raw(ptr2)}, unsafe {&Box::from_raw(ptr1)}, 2)
+    }) });
+
+    console_log!("Main: instanciated workloads for concurrency.");
+    
+    let res1 = execute_work(work1, &worker1);
+
+    let res2 = execute_work(work2, &worker2);
+
+    match res1.await {
+        Ok(()) => (),
+        Err(e) => return Err(e)
+    };
+
+    res2.await
+}
+
+async fn execute_work(work: Box<Work>, worker: &Worker) -> Result<(), JsValue> {
+    let ptr = Box::into_raw(work);
+    match worker.post_message(&JsValue::from(ptr as u32)) {
+        Ok(()) => Ok(()),
+        Err(e) => {
+            unsafe { drop(Box::from_raw(ptr)); }
+            crate::logv(&e);
+            Err(e)
+        }
+    }
+}
+
+fn deadlock_prone_task(first: &SyncronizedData, second: &SyncronizedData, worker: u32) -> Result<(), JsValue> {
+    console_log!("Worker {worker}: starting worker task ...");
+    let mut i = 0;
+    loop {
+        increment_decrement(first, second);
+        if i % 500 == 0 {
+            console_log!("Worker {worker}: {i} iterations done!")
+        } 
+        i += 1;
+    }
+    //console_log!("... finished!");
+    //Ok(())
+}
+
+struct SyncronizedData {
+    data: Mutex<i64>
+}
+
+impl SyncronizedData {
+    pub const fn new(initial: i64) -> Self {
+        Self { data: Mutex::new(initial) }
+    }
+
+    pub fn get_mut_data_ptr(&self) -> MutexGuard<'_, i64> {
+        self.data.lock().unwrap()
+    }
+}
+
+impl Default for SyncronizedData {
+    fn default() -> Self {
+        Self::new(0)
+    }
+}
+
+fn increment_decrement(first: &SyncronizedData, second: &SyncronizedData) {
+    let mut first_data = first.get_mut_data_ptr();
+    let mut second_data = second.get_mut_data_ptr();
+    *first_data += 1;
+    *second_data -= 1;
+    drop(first_data);
+    drop(second_data);
+}
+
+
